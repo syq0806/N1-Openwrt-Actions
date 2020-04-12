9c301853e1983129ebd659ba2e2cf4394c0fe7c1
syq0806 <61413903+syq0806@users.noreply.github.com>
2020-04-12 13:19:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,8 +5,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # Timezone
 sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
 sed -i "/timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
@@ -15,4 +13,4 @@ echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/c
 # Debug kpr adapt aarch64
 sed -i 's/,arm/,aarch64/' package/luci-app-koolproxyR/Makefile
 #修改版本号
-sed -i 's/OpenWrt/Bobotoy AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt/sunkist AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
